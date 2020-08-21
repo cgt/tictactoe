a7683a7eae1dc2ded695ccb826613c022a79b841
Christoffer G. Thomsen <chris@cgt.name>
2020-08-21 20:01:59 +0200

    checkpoint

--- test.lua
+++ test.lua
@@ -1,16 +1,26 @@
 lu = require 'luaunit'
 
 local function winner(board)
+	if board[1][1] == 'X' then
+		return 'X'
+	end
 	return nil
 end
 
 function testWinDetection()
 	emptyBoard = {
-		{'', '', '',},
-		{'', '', '',},
-		{'', '', '',},
+		{'', '', ''},
+		{'', '', ''},
+		{'', '', ''},
 	}
 	lu.assertNil(winner(emptyBoard))
+
+	fullRow = {
+		{'X', 'X', 'X'},
+		{'', '', ''},
+		{'', '', ''},
+	}
+	lu.assertEquals(winner(fullRow), 'X')
 end
 
 os.exit(lu.LuaUnit.run())
