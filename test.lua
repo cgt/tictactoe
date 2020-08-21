b328b231804434802f44cfd83492b574af5aa60e
Christoffer G. Thomsen <chris@cgt.name>
2020-08-21 20:16:05 +0200

    working

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ lu = require 'luaunit'
 
 local function winner(board)
 	for row, columns in ipairs(board) do
-		if columns[1] == 'X' then
+		if columns[1] == 'X' and columns[3] == 'X' then
 			return 'X'
 		end
 	end
@@ -37,6 +37,13 @@ function testWinDetection()
 		{'X', 'X', 'X'},
 	}
 	lu.assertEquals(winner(fullRow3), 'X')
+
+	twoMovesNoWinner = {
+		{'', '', ''},
+		{'', '', ''},
+		{'X', 'X', ''},
+	}
+	lu.assertNil(winner(twoMovesNoWinner))
 end
 
 os.exit(lu.LuaUnit.run())
